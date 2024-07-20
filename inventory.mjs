03b5f96bd8b6fd4021d2bf251443fd6b60e38886
rmdayberry <dayberryrm@gmail.com>
2024-07-20 11:36:27 -0500

    create inventory management system module
    create inventory management system module for JS Module Exercise

---
+++ inventory.mjs
@@ -0,0 +1,18 @@
+const inventory = [];
+
+export function addItem(item) {
+  inventory.push(item);
+  console.log(`${item} added to invetory.`);
+}
+
+export function removeItem(item) {
+  const index = inventory.indexOf(item);
+  if (index > -1) {
+    inventory.splice(index, 1);
+    console.log(`${item} removed from inventory`);
+  }
+}
+export function listItems() {
+  console.log("Inventory:");
+  inventory.forEach((item = console.log(item)));
+}
