90f843d68f742cc2ba2b603259d51188765c54ca
rmdayberry <dayberryrm@gmail.com>
2024-07-20 12:21:53 -0500

    Create app.mjs file and import functions from index
    Create app.mjs file, import functions from utils/index.mjs, and log to console use cases

---
+++ .history/app_20240720121913.mjs
@@ -0,0 +1,4 @@
+import { toLowerCase, add } from "./utils/index.mjs";
+
+console.log(toLowerCase("HELLO!"));
+console.log(add(5, 10));
---
+++ app.mjs
@@ -0,0 +1,5 @@
+import { toLowerCase, add, multiply } from "./utils/index.mjs";
+
+console.log(toLowerCase("HELLO!"));
+console.log(add(5, 10));
+console.log(multiply(10, 10));
